f6d9b0341e4685b06b703cfcb8b2d9a9260af388
Simone Bifani <simone.bifani@gmail.com>
2023-03-04 10:46:30 +0100

    cleanup

--- MdS_Modello_Predittivo/docs/sasdoc/directory_watcher.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-watchmedo shell-command \
-    --patterns="*.sas" \
-    --recursive \
-    --command='python sas2rst.py sas_source/ snd_attempt/ && make -C snd_attempt/ html' \
-    sas_source
